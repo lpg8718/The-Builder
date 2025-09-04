aa8b2561b0be9cd494a4ee936704b9b91bbc242c
Lalit patidar <lalitpatidar23102002@gmail.com>
2025-09-04 22:40:04 +0530

    Fix static files for Render

--- The_Builder/settings.py
+++ The_Builder/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'contractor',
 ]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]   # 👈 तुम्हारा static folder
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -118,8 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

252d92cf3bf5aa1f3633eeaf3b503519f310d598
Lalit patidar <lalitpatidar23102002@gmail.com>
2025-09-08 23:45:33 +0530

    add contractor home page

--- The_Builder/urls.py
+++ The_Builder/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path('contractor/', include('contractor.urls')),
 ]

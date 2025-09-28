16392ef4c09e0009c49a90465db80b333f5e01fc
Lalit patidar <lalitpatidar23102002@gmail.com>
2025-09-28 22:44:53 +0530

    change render to redirect from profile_edit to profile

--- contractor/views.py
+++ contractor/views.py
@@ -1,6 +1,6 @@
 import os
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from The_Builder.models import Users
 from The_Builder.settings import BASE_DIR
 from datetime import datetime
@@ -24,15 +24,15 @@ def contractore_edit_profile(request):
     if request.method=="POST":
         # You can access individual fields like this:
         try:
-            user_fullname = request.POST.get("fullname")
-            user_address = request.POST.get("address")  
-            user_profile_image = request.FILES.get("profile_picture")  # For file uploads
-            user_profile =request.POST.get("profile")
-            user_city = request.POST.get("city")
-            user_state = request.POST.get("state")
-            user_zip_code = request.POST.get("zip_code")
-            user_country = request.POST.get("country")
-            user_about_me = request.POST.get("about_me")
+            user_fullname = request.POST.get("fullname","")
+            user_address = request.POST.get("address","")  
+            user_profile_image = request.FILES.get("profile_picture","")  # For file uploads
+            user_profile =request.POST.get("profile","")
+            user_city = request.POST.get("city","")
+            user_state = request.POST.get("state","")
+            user_zip_code = request.POST.get("zip_code","")
+            user_country = request.POST.get("country","")
+            user_about_me = request.POST.get("about_me","")
             print(user_fullname, user_address, user_city, user_profile_image, user_state, user_zip_code, user_country, user_about_me)
             print("Profile id:", token_data["user_id"])
             ext=user_profile_image.name.split('.')[-1]
@@ -61,7 +61,7 @@ def contractore_edit_profile(request):
                 user_country=user_country,
                 user_about=user_about_me
             )
-            return render(request,'contractor_view_profile.html')
+            return redirect('/contractor/profile/')
         except Exception as e:
             print("Error updating profile:", e)
         # Here, you would typically save the updated profile information to the database.
